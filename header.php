7587cf52b52524f5d083da7008ff7103bd0a6f2d
priyarajendran06 <68416645+priyarajendran06@users.noreply.github.com>
2020-07-19 22:25:31 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,9 @@
+    <div id="header">
+      <div id="logo">
+        <div id="logo_text">
+          <!-- class="logo_colour", allows you to change the colour of the text -->
+          <h1><a href="index.php">Protein_<span class="logo_colour">names</span></a></h1>
+          <h2>with their description</h2>
+        </div>
+      </div>
+    </div>
